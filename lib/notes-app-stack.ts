b3121c472737347c8a3e15ffdd69919a4b06ac2b
lavirez <alirezaalavirazavi@yahoo.com>
2023-09-19 18:04:52 +0330

    Dummy Rest API with lambda

--- lib/notes-app-stack.ts
+++ lib/notes-app-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 import { Construct } from 'constructs';
 
 export class NotesAppStack extends Stack {
@@ -11,6 +12,10 @@ export class NotesAppStack extends Stack {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset("lambda"),
       handler: "hello.handler"
+    })
+
+    new apigw.LambdaRestApi(this, "Endpoint 1", {
+      handler: hello
     })
   }
 }
